83f17dfbc038e9e04e4fb8b43dc98bf162e2c100
Anas <Anassalah1421@gmail.com>
2024-05-04 12:26:29 +0300

    Adding rtl files

---
+++ rtl/CAVLC_TOP.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/27/2024 02:50:00 PM
+// Design Name: 
+// Module Name: CAVLC_TOP
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CAVLC_TOP(
+
+    );
+    
+    
+    
+    
+    
+    
+  clk_wiz_0_clk_wiz inst
+  (
+  // Clock out ports  
+  .clk_out1(clk_out1),
+  // Status and control signals               
+  .reset(reset), 
+  .locked(locked),
+ // Clock in ports
+  .clk_in1_p(clk_in1_p),
+  .clk_in1_n(clk_in1_n)
+  );
+    
+    
+    
+    
+    
+    
+    
+    
+endmodule
